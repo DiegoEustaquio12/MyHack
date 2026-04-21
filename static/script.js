d97f78341e982e5119603e74e314ea72a962464a
Rodri <rodridiego453@gmail.com>
2026-04-21 17:03:45 -0600

    Se hizo cambio en JS

--- static/script.js
+++ static/script.js
@@ -1,3 +1,21 @@
 console.log("JS listo");
 console.log('Equipo Listo')
 console.log("prueba1")
+
+document.getElementById("btn").addEventListener("click", async () => {
+    const cultivo = document.getElementById("cultivo").value;
+    const hectareas = document.getElementById("hectareas").value;
+
+    const respuesta = await fetch("/receta", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ cultivo, hectareas })
+    });
+
+    const datos = await respuesta.json();
+    document.getElementById("resultado").innerHTML = `
+        <p>🌱 Fertilizante: ${datos.fertilizante}</p>
+        <p>⚖️ Cantidad: ${datos.cantidad}</p>
+        <p>💰 Costo estimado: ${datos.costo}</p>
+    `;
+});
