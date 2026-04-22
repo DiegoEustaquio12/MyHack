3ed670a516c605982afe8382012f391d7b34c90b
Rodri <rodridiego453@gmail.com>
2026-04-22 00:55:52 -0600

    el programe se habre correctamente

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import webbrowser
+import threading
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,6 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Esto espera 1 segundo y abre la URL automáticamente
+threading.Timer(1.0, lambda: webbrowser.open("http://127.0.0.1:5000")).start()
 
 if __name__ == '__main__':
     app.run(debug=True)
