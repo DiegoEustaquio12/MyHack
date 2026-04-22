81c3373b0b055cc6357d4342814d311e777fea17
Rodri <rodridiego453@gmail.com>
2026-04-22 01:53:07 -0600

    Pestaña de Tiempo agregada e implementada correctamente

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import webbrowser
 import threading
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -9,14 +9,35 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# Esto espera 1 segundo y abre la URL automáticamente
+@app.route("/equipo")
+def equipo():
+    return "<h1> Somos 4 y ganaremos <h1>"
+
+@app.route('/receta', methods=['POST'])
+def calcular_receta():
+    
+    datos = request.get_json()
+    
+    cultivo = datos.get('cultivo')
+    hectareas = float(datos.get('hectareas')) 
+    tiempo = datos.get('tiempo')
+    
+    cantidad_calculada = hectareas * 150
+    costo_calculado = hectareas * 3500
+
+    
+    return jsonify({
+        "fertilizante": "Urea Premium y Fósforo",
+        "cantidad": f"{cantidad_calculada} kg",
+        "costo": f"${costo_calculado} MXN"
+    })
+
+# Esto espera 1 segundo y abre la URL de manera automatica 
 threading.Timer(1.0, lambda: webbrowser.open("http://127.0.0.1:5000")).start()
 
 if __name__ == '__main__':
     app.run(debug=True)
     
     
-@app.route("/equipo")
-def equipo():
-    return "<h1> Somos 4 y ganaremos <h1>"
+
 
--- static/script.js
+++ static/script.js
@@ -5,17 +5,32 @@ console.log("prueba1")
 document.getElementById("btn").addEventListener("click", async () => {
     const cultivo = document.getElementById("cultivo").value;
     const hectareas = document.getElementById("hectareas").value;
+    const tiempo = document.getElementById("tiempo").value;
 
-    const respuesta = await fetch("/receta", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ cultivo, hectareas })
-    });
+    if (!hectareas) {
+        alert("Ingresa el numero de Hectareas");
+        return;
 
-    const datos = await respuesta.json();
-    document.getElementById("resultado").innerHTML = `
-        <p>🌱 Fertilizante: ${datos.fertilizante}</p>
-        <p>⚖️ Cantidad: ${datos.cantidad}</p>
-        <p>💰 Costo estimado: ${datos.costo}</p>
-    `;
+    }
+
+    try {
+        
+        const respuesta = await fetch("/receta", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ tiempo: tiempo, cultivo: cultivo, hectareas: hectareas })
+        });
+
+        
+        const datos = await respuesta.json();
+        document.getElementById("resultado").innerHTML = `
+            <p>🌱 Fertilizante: ${datos.fertilizante}</p>
+            <p>⚖️ Cantidad: ${datos.cantidad}</p>
+            <p>💰 Costo estimado: ${datos.costo}</p>
+        `;
+    } catch (error) {
+        console.error("Hubo un error con el servidor:", error);
+        document.getElementById("resultado").innerHTML = "<p>Hubo un error al calcular la receta.</p>";
+    }
+    
 });
