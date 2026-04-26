4245b0ade9b8273240fb561c97d454696da4c376
Croldtac <isaac.sandovaal@outlook.com>
2026-04-26 06:09:32 -0600

    Corrección de errores Routes

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,7 @@
 from flask import Blueprint, jsonify, render_template
-from .models import Municipio
+from .models import Cultivo, Idoneo, MunicipioCultivo
 
 main = Blueprint("main", __name__)
-# Crea un grupo de rutas
 
 @main.route("/")
 def inicio():
